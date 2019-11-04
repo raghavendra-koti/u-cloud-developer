17e88ab44e4703e88da9a3d613d8a14d18aa5fd6
Raghavendra Koti <raghavendra.koti@gmail.com>
2019-11-04 23:47:09 +0530

    first cut ci/cd

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,15 +11,15 @@ if [ "$1" == "BUILD" -o "$1" == "DEPLOY" ]
 then
     docker -v 
     #&& docker-compose -v
-    docker build -t udacity-restapi-feed ../course-03/exercises/udacity-c3-restapi-feed/.
+    docker build -t udacity-restapi-feed course-03/exercises/udacity-c3-restapi-feed/.
     docker tag udacity-restapi-feed shaastra/udacity-restapi-feed:"$VERSION" 
 
-    docker build -t udacity-restapi-user ../course-03/exercises/udacity-c3-restapi-user/.
+    docker build -t udacity-restapi-user course-03/exercises/udacity-c3-restapi-user/.
     docker tag udacity-restapi-user shaastra/udacity-restapi-user:"$VERSION"
     
-    docker build -t udacity-frontend ../course-03/exercises/udacity-c3-frontend/.
+    docker build -t udacity-frontend course-03/exercises/udacity-c3-frontend/.
     docker tag udacity-frontend shaastra/udacity-frontend:"$VERSION"
     
-    docker build -t reverseproxy ../course-03/exercises/udacity-c3-deployment/docker/.
+    docker build -t reverseproxy course-03/exercises/udacity-c3-deployment/docker/.
     docker tag reverseproxy shaastra/reverseproxy:"$VERSION"
 fi
