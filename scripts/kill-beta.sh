c9f574d4463ca1015ac4c1d45b6fa03a4b69683c
Raghavendra Koti <raghavendra.koti@gmail.com>
2019-11-05 03:05:49 +0530

    first cut ci/cd

--- scripts/kill-beta.sh
+++ scripts/kill-beta.sh
@@ -1,4 +1,5 @@
  #!/usr/bin/bash
+
  kubectl delete deployment "backend-feed-beta" --grace-period=60
  kubectl delete deployment "backend-user-beta" --grace-period=60
  kubectl delete deployment "frontend-beta" --grace-period=60
